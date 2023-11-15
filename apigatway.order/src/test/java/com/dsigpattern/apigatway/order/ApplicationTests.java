28838efdbc6f5ec7b84cce86cd3cc194db69cce8
oArth <obulix@hotmail.com>
2023-11-14 21:08:51 -0600

    Initial project checkins

---
+++ apigatway.order/src/test/java/com/dsigpattern/apigatway/order/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsigpattern.apigatway.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
